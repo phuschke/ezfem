d931fba5d74a27f321911b8178b888506598de84
phuschke <philiphuschke@gmail.com>
2016-11-04 12:38:29 +0100

    Check if MPI_Allgatherv works

--- test/first_test.cpp
+++ test/first_test.cpp
@@ -2,7 +2,8 @@
 #include <boost/mpi.hpp>
 #include <iostream>
 
-
+#include <vector>
+#include <eigen3/Eigen/Dense>
 
 
 int main(int argc, char* argv[])
@@ -19,9 +20,41 @@ int main(int argc, char* argv[])
 
     std::cout << "4 x 1 = " << a << std::endl;
 
+    std::vector<int> recvCount;
+    recvCount.push_back(1);
+    recvCount.push_back(2);
+    recvCount.push_back(1);
+    recvCount.push_back(0);
+
+    std::vector<int> displ;
+    displ.push_back(0);
+    displ.push_back(1);
+    displ.push_back(3);
+    displ.push_back(4);
 
 
+    Eigen::VectorXd vector;
+    vector.setConstant(3, world.rank());
 
+    Eigen::VectorXd vectorGlobal;
+    vectorGlobal.setZero(6);
+
+    std::cout << vector << std::endl;
+
+    world.barrier();
+
+    MPI_Allgatherv(vector.data(),
+                   vector.size(),
+                   MPI_DOUBLE,
+                   vectorGlobal.data(),
+                   recvCount.data(),
+                   displ.data(),
+                   MPI_DOUBLE,
+                   MPI_COMM_WORLD);
+
+
+    if (world.rank()== 0)
+        std::cout << vectorGlobal << std::endl;
 
 
     std::cout << "test successful" << std::endl;
