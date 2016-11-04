7c85ee5df5e8e59d085d9879feda7185348231c8
phuschke <philiphuschke@gmail.com>
2016-11-04 09:24:26 +0100

    Test boost::mpi::all_reduce

--- test/first_test.cpp
+++ test/first_test.cpp
@@ -10,7 +10,22 @@ int main(int argc, char* argv[])
     boost::mpi::environment  env(argc, argv);
     boost::mpi::communicator world;
     std::cout << world.rank() << std::endl;
+
+
+
+    int a = 1;
+    boost::mpi::all_reduce(world,boost::mpi::inplace<int>(a), std::plus<int>());
+
+
+    std::cout << "4 x 1 = " << a << std::endl;
+
+
+
+
+
+
     std::cout << "test successful" << std::endl;
+
 
 
 
