08057d06808342ff767525ec2412792491bb6774
phuschke <philiphuschke@gmail.com>
2016-11-07 15:11:30 +0100

    ...

--- travis_script.sh
+++ travis_script.sh
@@ -2,7 +2,7 @@ wget -O boost_1_61_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.
 tar xfz boost_1_61_0.tar.gz
 cd boost_1_61_0/
 ./bootstrap.sh --prefix=/usr/local
-echo 'using mpi : <find-shared-library>mpic++ ;' >> project-config.jam
+echo 'using mpi : /usr/bin/mpicxx ;' >> project-config.jam
 cat project-config.jam
 sudo ./b2 --with-mpi --with-serialization -d0 -q install
 cd ..
